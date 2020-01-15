ca989c6694cb6567148468b312b24c138e67591e
breaker2l <34549956+breaker2l@users.noreply.github.com>
2020-01-14 22:28:05 -0800

    blog

---
+++ entries/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView,CreateView
+from .models import Entry
+
+# Create your views here.
+
+class HomeView(ListView):
+    model = Entry
+    template_name = 'entries/index.html'
+    context_object_name = "blog_entries"
+
+class EntryView(DetailView):
+    model = Entry 
+    template_name = 'entries/entry_detail.html'
+    
+class CreateEntryView(CreateView):
+    model = Entry
+    template_name = 'entries/create_entry.html'
+    fields = ['entry_title','entry_text']
+
