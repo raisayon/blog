137508bc67acfca032cbfbef921756e36f8fb91f
breaker2l <34549956+breaker2l@users.noreply.github.com>
2020-01-17 19:42:40 -0800

    added ordering

--- entries/views.py
+++ entries/views.py
@@ -8,6 +8,8 @@ class HomeView(ListView):
     model = Entry
     template_name = 'entries/index.html'
     context_object_name = "blog_entries"
+    ordering = ['-entry_date']
+    pagination_by = 3
 
 class EntryView(DetailView):
     model = Entry 
@@ -18,3 +20,6 @@ class CreateEntryView(CreateView):
     template_name = 'entries/create_entry.html'
     fields = ['entry_title','entry_text']
 
+    def form_valid(self,form):
+        form.instance.entry_author = self.request.user
+        return super().form_valid(form)
---
+++ users/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .forms import RegistrationForm
+
+# Create your views here.
+
+def register(request):
+    if request.method =='POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = RegistrationForm()
+        
+    context = {'form':form}
+    return render(request,'users/register.html',context)
